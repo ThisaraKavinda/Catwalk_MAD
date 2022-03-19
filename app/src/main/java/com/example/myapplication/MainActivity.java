72766945f07d8796bf0e1a692bd91664b507f6b3
Nikalo-Flamel <71638740+Nikalo-Flamel@users.noreply.github.com>
2022-03-20 05:17:14 +0530

    Created Client History view page

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClientRequestedModelsList.class);
+                Intent intent = new Intent(MainActivity.this, ClientViewHistory.class);
                 startActivity(intent);
             }
         });
