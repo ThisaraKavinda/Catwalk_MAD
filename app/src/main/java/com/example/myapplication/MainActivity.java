1f126af7e6cb56cd03e638265b998b7fa7284364
hdplgunasekara <shanugunasekara11@gmail.com>
2022-03-19 00:32:40 -0700

    MAD Project Catwalk

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button move;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        move = (Button) findViewById(R.id.move);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdminHome.class);
+                startActivity(intent);
+            }
+        });
+        
+    }
+}
