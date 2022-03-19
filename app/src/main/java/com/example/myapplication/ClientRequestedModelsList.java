1dba400b18edc7818eb7102f3d2ac320c0f094d0
Nikalo-Flamel <71638740+Nikalo-Flamel@users.noreply.github.com>
2022-03-20 04:21:00 +0530

    created the Client view available model list page

---
+++ app/src/main/java/com/example/myapplication/ClientRequestedModelsList.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientRequestedModelsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_requested_models_list);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ClientViewAd.class);
+                Intent intent = new Intent(MainActivity.this, ClientRequestedModelsList.class);
                 startActivity(intent);
             }
         });
