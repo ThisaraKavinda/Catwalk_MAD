66258cda3b26d90d10cc476d34c99b6ca2dcd400
Nikalo-Flamel <71638740+Nikalo-Flamel@users.noreply.github.com>
2022-03-19 16:54:49 +0530

    Before deleting fragment files

--- app/src/main/java/com/example/myapplication/ModelRequestActivity.java
+++ app/src/main/java/com/example/myapplication/ModelRequestActivity.java
@@ -17,35 +17,9 @@ import com.example.myapplication.databinding.ActivityModelRequestBinding;
 
 public class ModelRequestActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityModelRequestBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityModelRequestBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        setContentView(R.layout.activity_model_request);
     }
 }
