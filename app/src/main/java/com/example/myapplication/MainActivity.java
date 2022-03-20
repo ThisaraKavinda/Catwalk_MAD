17eafa484e7a00aba3a2e0de8f573382c7d4f337
Nikalo-Flamel <71638740+Nikalo-Flamel@users.noreply.github.com>
2022-03-20 13:27:37 +0530

    created update inquiry page

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ModelArticleList.class);
+                Intent intent = new Intent(MainActivity.this, InquiryUpdate.class);
                 startActivity(intent);
             }
         });
